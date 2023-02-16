4ebf1812a8dc390fb56631638047922e2118d42f
Manu-Estrada <mirucuxusticieru@gmail.com>
2023-02-16 11:26:49 +0100

    creando proyecto

---
+++ src/main/java/com/events/upcoming/UpcomingApplication.java
@@ -0,0 +1,13 @@
+package com.events.upcoming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcomingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpcomingApplication.class, args);
+	}
+
+}
