b61a28ebb401f197473f6d8a33516fda8f48bea6
Andrespz07 <andresperezortega2001@gmail.com>
2023-02-17 15:32:45 +0100

    CardRecomendados

--- src/main/frontend/upcoming-events/src/App.vue
+++ src/main/frontend/upcoming-events/src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import CardsRecomendados from './components/CardsRecomendados.vue';
 
 </script>
 
 <template>
-
+<CardsRecomendados></CardsRecomendados>
 </template>
 
 <style scoped>
---
+++ src/main/frontend/upcoming-events/src/components/CardsRecomendados.vue
@@ -0,0 +1,47 @@
+<script setup>
+</script>
+
+<template>
+    <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="align-end text-white"
+      height="200"
+      cover
+    >
+    <img src="../assets/img/ConciertoBrillo.png" alt="Concierto" id="concierto">
+
+      <v-card-title>Nuestros recomendados</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pt-4">
+      Number 10
+    </v-card-subtitle>
+
+    <v-card-text>
+      <div>Whitehaven Beach</div>
+
+      <div>Whitsunday Island, Whitsunday Islands</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn color="orange">
+        Share
+      </v-btn>
+
+      <v-btn color="orange">
+        Explore
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+
+</template>
+
+<style lang="scss" scoped>
+    #concierto{
+        height: 200px;
+        width: 400px;
+    }
+</style>
