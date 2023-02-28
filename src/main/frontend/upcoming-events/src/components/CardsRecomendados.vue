d75f61ede7de3e8ae3466c192a97db84739f3d10
Andrespz07 <andresperezortega2001@gmail.com>
2023-02-28 15:20:46 +0100

    Component CardRecomendados.vue done

--- src/main/frontend/upcoming-events/src/components/CardsRecomendados.vue
+++ src/main/frontend/upcoming-events/src/components/CardsRecomendados.vue
@@ -16,7 +16,7 @@
     >
     <img src="../assets/img/ConciertoBrillo.png" alt="Concierto" id="photo" class="CardSize">
 
-      <v-card-subtitle id="date" class="text-subtitle-1">10/12/2023 - 21:30</v-card-subtitle>
+      <v-card-subtitle id="date">10/12/2023 - 21:30</v-card-subtitle>
     </v-img>
 
     <v-card-title class="pt-4" id="EventTitle">
@@ -32,7 +32,7 @@
     </v-card-text>
 
     <v-card-actions>
-      <v-btn color="orange" id="Apuntame">
+      <v-btn  id="Apuntame">
         ¡Apúntame!
       </v-btn>
 
@@ -42,23 +42,39 @@
 </template>
 
 <style lang="scss" scoped>
-
+@import '../assets/scss/Variables.scss';
 
   #Card{
     font-family: Dosis;
+    background-color: $Yellow;
     #photo{
         height: 200px;
         width: 400px;
+    }
+    #date{
+      color: $Blue;
+      font-weight: 800;
+      opacity: 100;
+    }
+    #EventTitle{
+      color: $Blue;
+      padding-top: 0 !important;
+    }
+    .EventDescription{
+      font-size: 1.1em;
     }
     .VerMas{
       display: flex;
       justify-content: flex-end;
     }
     .Quotas{
-      margin: auto;
+      display: flex;
+      justify-content: center;
     }
     #Apuntame{
       margin: auto;
+      color: $White;
+      background-color: $Blue;
     }
     }
 </style>
