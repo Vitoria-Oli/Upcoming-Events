39d89a7f693bcee0f40fc998d9b7b3c4f9c5231d
JohnnyRamirezSancho <info@johnnyramirez.es>
2023-02-28 15:24:19 +0100

    UserEvent > get, post and delete

---
+++ src/main/java/com/events/upcoming/services/UserEventService.java
@@ -0,0 +1,32 @@
+package com.events.upcoming.services;
+import org.springframework.stereotype.Service;
+import com.events.upcoming.models.UserEvent;
+import com.events.upcoming.repositories.UserEventRepository;
+
+
+
+
+import java.util.List;
+
+@Service
+public class UserEventService {
+    private UserEventRepository repository;
+
+    public UserEventService(UserEventRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<UserEvent> getAll() {
+        return repository.findAll();
+    }
+
+    public UserEvent save(UserEvent userevent){
+        UserEvent newuserevent=repository.save(userevent);
+        return newuserevent;
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+}
