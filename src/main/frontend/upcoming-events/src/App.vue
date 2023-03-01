3f4c64662aa7ecb1252ccdf1c120921aa58efe9f
Vitoria Oliveira <vitoria.oli.san14@gmail.com>
2023-03-01 14:48:04 +0100

    create footer

--- src/main/frontend/upcoming-events/src/App.vue
+++ src/main/frontend/upcoming-events/src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
 import Header from './components/Header.vue';
-import CardsEvents from './components/CardsEvents.vue'
+import CardsEvents from './components/CardsEvents.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 
 <template>
 <Header></Header>
 <CardsEvents></CardsEvents>
+<Footer></Footer>
 </template>
 
 <style scoped>
