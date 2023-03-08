73a92c5df2659c0c875dbe06dcc6c80564c5e80f
Andrespz07 <andresperezortega2001@gmail.com>
2023-03-08 15:17:14 +0100

    DeleteAll.vue

---
+++ src/main/frontend/upcoming-events/src/components/DeleteAll.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+<button class="btn btn-success">Borrar todos</button>
+</template>
+
+<style lang="scss" scoped>
+@import '../assets/scss/Variables.scss';
+
+button{
+    font-family: Dosis;
+    background-color: $Red;
+}
+    
+</style>
