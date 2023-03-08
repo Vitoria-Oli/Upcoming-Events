8bfd628b0df3bff89329554eb8e6560f214f7995
guillermo <h.uhm292@gmail.com>
2023-03-08 14:30:38 +0100

    finish addevents and modifyevents

--- src/main/frontend/upcoming-events/src/views/ModifyEvents.vue
+++ src/main/frontend/upcoming-events/src/views/ModifyEvents.vue
@@ -5,86 +5,55 @@ import Footer from "../components/Footer.vue";
 
 <template>
   <Header></Header>
-  <h1>Modificar Evento</h1>
-  
-  
+  <h1>Modify Evento</h1>
+
+
   <form id="form">
-    
-    <div class = "formtwo">
+
+    <div class="formtwo">
 
       <div class="dateFields">
         <label for="name"><span class="Asterisk">* </span>Introduce la fecha</label>
-        <input
-        type="date"
-        class="form-control"
-        id="date"
-        />
+        <input type="date" class="form-control" id="date" />
       </div>
       <div class="dateFields">
         <label for="name"><span class="Asterisk">* </span>Añade la hora</label>
-        <input
-        type="text"
-        class="form-control form-control-lg"
-        id="hora"
-        />
+        <input type="text" class="form-control form-control-lg" id="hora" />
       </div>
       <div class="check">
-        <input
-        type="checkbox"
-        />
+        <input type="checkbox" class="prueba" />
         <label for="name"> Recomendado</label>
       </div>
     </div>
-       
-    
+
+
     <div class="form-group">
       <label for="name"><span class="Asterisk">* </span>Introduce un titulo</label>
-      <input
-        type="text"
-        class="form-control form-control-lg"
-        id="titulo"
-        />
-        
+      <input type="text" class="form-control form-control-lg" id="titulo" />
+
     </div>
 
     <div class="form-group">
       <label for="name"><span class="Asterisk">* </span>Añade una descripción</label>
-      <input
-        type="text"
-        class="form-control form-control-lg"
-        id="description"
-        />
-        
+      <input type="text" class="form-control form-control-lg" id="description" />
+
     </div>
 
     <div class="form-group">
       <label for="name"><span class="Asterisk">* </span>Determina el número de plazas</label>
-      <input
-        type="number"
-        class="form-control form-control-lg"
-        id="plazas"
-      />
+      <input type="number" class="form-control form-control-lg" id="plazas" />
     </div>
 
     <div class="form-group">
       <label for="name"><span class="Asterisk">* </span>Añade una imagen</label>
-      <input
-         type="file"
-        class="form-control form-control-lg"
-        id="file"
-      />
+      <input type="file" class="form-control form-control-lg" id="file" />
     </div>
-    
+
     <div id="buttons-box">
-      <button
-        type="button"
-        class="btn btn-warning"
-        id="reset"
-        @click="resetForm()"
-      >
+      <button type="button" class="btn btn-warning" id="reset" @click="resetForm()">
         Borrar
       </button>
-      <button type="button" class="btn btn-success" id="send" @click="save()">
+      <button type="button" class="btn btn-success btn-add" id="send" @click="save()">
         Añadir
       </button>
     </div>
@@ -95,61 +64,112 @@ import Footer from "../components/Footer.vue";
 <style lang="scss" scoped>
 @import "../assets/scss/Variables.scss";
 
-style> 
-  #form{
-  width: 90%;
-}
-label{
-        font-weight: bold;
-        font-size: 1.4em;
-        .Asterisk{
-        color: $Red;
-      }
-      }
 
-      h1{
+#form {
+  width: 90%;
+  margin: auto;
+}
+
+label {
+  font-weight: bold;
+  font-size: 1.4em;
+
+  .Asterisk {
+    color: $Red;
+  }
+}
+
+h1 {
   font-family: Dosis;
   color: $Blue;
   font-weight: bold;
   padding: 40px;
 
 }
- .formtwo{
+
+.formtwo {
   display: flex;
-  
-  .dateFields{
+  justify-content: space-between;
+
+  .dateFields {
     display: flex;
-    // flex-direction: column;
     flex-wrap: wrap;
-    margin-left:30px;
-    
-     
   }
   
-  
+
+
 }
-#form{
-  display:flex;
+
+#form {
+  display: flex;
   flex-direction: column;
+  .form-group{
+    margin: 1% 0 1% 0;
+  }
 }
-input{
+
+input {
   height: 50px;
   border: solid 2px $Blue;
 }
 
-#buttons-box{
+#buttons-box {
   display: flex;
   align-self: end;
   margin-top: 20px;
   margin-right: 40px;
 }
 
-.check{
+.check {
   display: flex;
   flex-direction: row;
-  align-content: end;
   margin-left: 130px;
-  margin-top: 50px;
+  margin-top: 33px;
+  label{
+    line-height: 50px;
+  }
+  .prueba{
+    appearance: none;
+    display: inline-flex;
+  align-items: center;
+  cursor: pointer;
+  background-color: $White;
+  color: $White;
+  background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M12.207 4.793a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-2-2a1 1 0 011.414-1.414L6.5 9.086l4.293-4.293a1 1 0 011.414 0z'/%3e%3c/svg%3e");
+  width: 50px;
+  height: 50px;
+  border: 2px solid $Blue;
+  background-position: 0 -2rem;
+  background-size: 100%;
+  background-repeat: no-repeat;
+  transition: all 0.3s ease-in-out;
+  border-radius: 5px;
+  margin-right: 10px;
+  &:checked {
+    background-color: $Blue;
+    color: $Blue;
+    background-position: 0 0;
+  }
 }
 
+    }
+  
+.btn-add {
+  margin-left: 2em;
+}
+@media (max-width:900px){
+  .formtwo {
+  flex-direction: column;
+  }
+  .check {
+  margin-top: 15px;
+  margin-left: 0px;
+  }
+  
+
+    }
+
 </style>
+
+
+
