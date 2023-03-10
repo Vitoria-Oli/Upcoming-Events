8641ded943531b01282d011dfb998df76319367f
Manu-Estrada <mirucuxusticieru@gmail.com>
2023-03-10 15:27:48 +0100

    admin events

--- src/main/frontend/upcoming-events/src/views/AdminEvents.vue
+++ src/main/frontend/upcoming-events/src/views/AdminEvents.vue
@@ -3,32 +3,83 @@ import { RouterLink } from "vue-router";
 import Header from "../components/Header.vue";
 import Footer from "../components/Footer.vue";
 import CardsEvents from "../components/CardsEvents.vue";
+import { useEventsStore } from "../stores/Events";
+import { onBeforeMount } from "vue";
+import AddEventButton from "../components/AddEventButton.vue"
+import CloseSessionButton from "../components/CloseSessionButton.vue"
+
+const store = useEventsStore();
+onBeforeMount(async () => {
+  await store.fetchEvents();
+});
+
 </script>
 
 <template>
 <Header></Header>
-    <div class="fulanito">
-      <h2>Bienvenido, Fulanito.<br>
-      <span>Listado de eventos:</span>
-    </h2>
-    </div>
+     <div id="TitleAndButton">
+      <h1>Bienvenido, Fulanito.<br>
+        <h2><span>Listado de eventos:</span></h2>
+      </h1>
+    <div id="ContainerButton"> 
+         <button type="button" class="btn btn-sucess btn-add" id="reset" @click="save()">
+            Añadir<br>Evento
+          </button>
+          <button type="button" class="btn btn-success btn-add" id="send" @click="save()">
+            Cerrar<br>Sesión
+          </button>
+          </div>
+     </div>
+    
     <div id="EventsContainer">
-        <h2>Todo lo que puedes ver y hacer:</h2>
         <div class="eventos">
           <CardsEvents v-for="event in store.Events" :event="event"></CardsEvents>
-        </div>
-      </div>
+        </div> 
+       </div> 
 
 
     
 <Footer></Footer>
 
-
-
-    
-    
-
-
-
-
 </template>
+
+<style lang="scss" scoped>
+@import "../assets/scss/Variables.scss";
+*{
+    font-family: Dosis;
+    #TitleAndButton{
+        display: flex;
+        justify-content: space-between;
+        margin-top: 2vw;
+
+        
+      #ContainerButton{
+        display: flex;
+        align-self: end;
+        margin-top: 20px;
+      }
+    }
+h1  {
+    color: $Blue;
+    margin-top: 40px;
+    margin-left: 8%;
+    margin-bottom: 20px;
+    font-weight: 700;
+  }
+h2 {
+    margin-top: 5px;
+    
+}
+    
+    
+    font-weight: 700;
+  } 
+  .btn-add {
+    margin-left: 1em;
+  }
+
+    
+
+
+
+</style>
