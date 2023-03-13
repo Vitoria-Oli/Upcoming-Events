93e2f1a65e9b6277341c9cf4a84c947df83a2b48
Vitoria Oliveira <vitoria.oli.san14@gmail.com>
2023-03-13 15:01:55 +0100

    edit root

--- src/main/frontend/upcoming-events/src/components/CardsEvents.vue
+++ src/main/frontend/upcoming-events/src/components/CardsEvents.vue
@@ -1,19 +1,16 @@
 <script setup>
 import { onBeforeMount } from "vue";
 import { useUsersStore } from "../stores/Users";
-
 const store = useUsersStore();
 onBeforeMount(async () => {
   await store.fetchUsers("3");
 });
-
 const props = defineProps({
   event: {
     type: Object,
   },
 });
 </script>
-
 <template>
   <v-card class="mx-auto card">
     <div class="headerCard">
@@ -39,43 +36,6 @@ const props = defineProps({
         </v-card-text>
       </div>
     </div>
-
-      <!-- Modal -->
-      <div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-body">
-              <v-img >
-                <img class="modal-img" src="../assets/img/Violinista.png" alt="" />
-              </v-img>
-              <div class="modal-info">
-               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> 
-               <v-card-subtitle class="date">{{ event.date }} - {{ event.hour }}</v-card-subtitle>
-                <v-card-title class="pt-4 eventTitle">
-                  {{ event.name }}
-                </v-card-title>
-                <v-card-text class="nomargin">
-                  <div class="eventDescription">
-                    <p>{{ event.description }}</p>
-                  </div>
-                </v-card-text>
-                <div class="footerCard">
-                <v-card-text class="nomargin">
-                  <div class="quotas">
-                    <p class="nomargin">
-                      Quedan <b>12</b> plazas de <b>{{ event.capacity }}</b>
-                    </p>
-                  </div>
-                </v-card-text>
-                <v-card-actions>
-                  <v-btn class="apuntame">¡Apúntame!</v-btn>
-                </v-card-actions>
-              </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     <button
       type="button"
       class="btn BtnPopUp"
@@ -97,7 +57,6 @@ const props = defineProps({
       </v-card-actions>
     </div>
   </v-card>
-
     <div
       class="modal fade"
       :id="`event${event.id}`"
@@ -141,49 +100,38 @@ const props = defineProps({
       </div>
     </div>
 </template>
-
 <style lang="scss" scoped>
 @import "../assets/scss/Variables.scss";
-
 .card {
   font-family: Dosis;
   background-color: $Yellow;
   margin: 20px !important;
   min-width: 420px;
-
-
   &:hover {
     box-shadow: 8px 8px 8px rgba(128, 128, 128, 0.5);
   }
-
   .nomargin {
     margin-bottom: 0;
     padding-bottom: 0;
   }
-
   .headerCard {
     display: flex;
-
     .photo {
       transition: all 0.4s;
       width: 164px;
       height: 164px;
       object-fit: cover;
-
       &:hover {
         transform: scale(1.1);
       }
     }
-
     .headerTextcard {
       width: calc(100% - 160px);
-
       .date {
         color: $Blue;
         font-weight: bold;
         margin-top: 10px;
       }
-
       .eventTitle {
         color: $Blue;
         padding-top: 0 !important;
@@ -191,14 +139,12 @@ const props = defineProps({
         margin-bottom: 0;
         padding-bottom: 0;
       }
-
       .eventDescription p {
         font-size: 16px;
         margin-bottom: 3px;
       }
     }
   }
-
   .BtnPopUp {
     display: flex;
     justify-content: flex-end;
@@ -212,79 +158,14 @@ const props = defineProps({
     &:hover {
       text-decoration: underline;
     }
-
-.modal-dialog{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 60vw;
-    .modal-content{
-      background-color: $White;
-      width: 60vw;
-      
-        .modal-body{
-           width: 60vw;
-           display: flex;
-           justify-content: space-between; 
-           align-items: center;
-
-          .btn-close{
-          background-color: $Blue;
-          display: flex;
-          opacity: 100%;
-          margin: 3% 0 0 92%;
-          }
-            .v-img{
-              width: 20vw;
-              height: 44vh;
-              margin-right: -4vw;
-                .modal-img{
-                  height: 100%;
-                  width: 70%;
-                  border-radius: 5%;
-                }
-
-            }
-            .modal-info{
-              background-color: $Yellow;
-              height: 44vh;
-              width: 100vw;
-              border-radius: 5%;
-
-              .date {
-              color: $Blue;
-              font-size: 1.4rem;
-              }
-              .eventTitle{
-              color: $Blue;
-              font-weight: bold;
-              font-size: 2vw;
-            }
-            .eventDescription p {
-            font-size: 1.3rem;
-            margin-bottom: 3px;
-          }
-           .footerCard{
-            margin-right: 3rem;
-
-            .quotas{
-              font-size: 1.6rem;
-            }
-           }
-        }
-      }
-    }
   }
-
   .footerCard {
     display: flex;
     margin-bottom: 10px;
-
     .quotas {
       font-size: 20px;
       text-align: right;
     }
-
     .apuntame {
       color: $White;
       background-color: $Blue;
@@ -292,12 +173,11 @@ const props = defineProps({
       font-weight: bold;
       font-variant: initial !important;
       margin-right: 7px;
-
       &:hover {
         color: $Blue;
         background-color: $White;
       }
     }
   }
-}}
+}
 </style>
