f1e324fb51efe74599af70b8dc49ca4788626f49
Johnny Ramírez <116891736+JohnnyRamirezSancho@users.noreply.github.com>
2023-03-15 10:08:00 +0100

    Revert "Frontend"

--- src/main/frontend/upcoming-events/src/components/Footer.vue
+++ src/main/frontend/upcoming-events/src/components/Footer.vue
@@ -1,173 +1,90 @@
 <script setup>
-import { computed } from "@vue/reactivity";
 
-const date = computed({
-  get() {
-    let today = new Date();
+window.onload = function formDate() {
+  let today = new Date();
 
-    let day = today.getDate();
-    let month = today.getMonth() + 1;
-    let year = today.getFullYear();
+  let day = today.getDate();
+  let month = today.getMonth() + 1;
+  let year = today.getFullYear();
 
-    day = ("0" + day).slice(-2);
-    month = ("0" + month).slice(-2);
+  day = ('0' + day).slice(-2);
+  month = ('0' + month).slice(-2);
 
-    return `${day}/${month}/${year}`;
-  },
-});
+  let printDate = `${day}/${month}/${year}`
+  document.getElementById("date").innerHTML = printDate
+
+};
+
 </script>
 
 <template>
-  <footer>
-    <figure>
-      <img
-        class="logoFooter"
-        src="../assets/img/happiness-blanco-1.png"
-        alt="Happiness & Co."
-      />
-    </figure>
-    <div class="infoFooter">
-      <img
-        id="slogan"
-        src="../assets/img/happiness-blanco-2.png"
-        alt="Happiness & Co."
-      />
-      <p class="date">{{ date }}</p>
-      <p>Contáctanos: <a href="#">somostu@happinessandco.com</a></p>
+<footer>
+    <img id="Logo" src="../assets/img/happiness-blanco-1.png" alt="">
+    <div id="Info">
+        <img id="Slogan" src="../assets/img/happiness-blanco-2.png" alt="">
+        <label for="date" id="date"></label >
+        <p >Contáctanos: somostu@happinessandco.com</p>
     </div>
-    <div id="nav">
-      <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/UserRegister">Registrate</router-link></li>
-        <li><router-link to="/AddEvents">Zona de usuario</router-link></li>
-        <li>
-          <router-link to="/AddEvents">Zona de administración</router-link>
-        </li>
-      </ul>
+    <div id="Nav">
+        <ol>
+            <li>Home</li>
+            <li>Registrate</li>
+            <li>Zona de usuario</li>
+            <router-link to="/AddEvents"><li>Zona de administración</li></router-link>
+        </ol>
     </div>
-  </footer>
+</footer>
 </template>
 
 <style lang="scss" scoped>
-@import "../assets/scss/Variables.scss";
+@import '../assets/scss/Variables.scss';
 
-footer {
-  background-color: $Blue;
-  display: flex;
-  flex-direction: row;
-  justify-content: start;
-  width: 100%;
-  height: auto;
-  margin-top: 50px;
+footer{
+background-color: $Blue;
+display: flex;
+flex-direction: row;
+justify-content: space-around;
+width: 100%;
+margin-top: 50px;
 
-  figure {
-    margin: 0;
-    margin-left: 3vw;
-  }
+ #Logo{
+    display: flex;
+    justify-content: start;
+    margin-left: 0%;
+ }
 
-  .infoFooter {
+ #Info{
     display: flex;
     flex-direction: column;
     justify-content: center;
-    margin-left: 7vw;
-
-    img {
-      width: 26vw;
+    margin-left: 5%;
+    #Slogan{
+        width: 20vw;
     }
 
-    label {
-      font-family: Dosis;
-      font-weight: 800;
-      color: $White;
-      font-size: 4vw;
+    label{
+        font-family: Dosis;
+        font-weight: 800;
+        color: $White;
+        font-size: 4vw;
     }
 
-    p {
-      font-family: Dosis;
-      color: $White;
-      font-weight: bold;
+    p{
+        font-family: Dosis;
+        font-weight: 800;
+        color: $White;
     }
-    .date {
-      font-size: 3.6vw;
-      line-height: 3.5vw;
-      font-weight: normal;
-    }
-    a {
-      text-decoration: none;
-      color: $White;
-      &:hover {
-        text-decoration: underline;
-      }
-    }
-  }
-
-  #nav {
-    font-family: Dosis;
-    font-weight: normal;
-    color: $White;
-    margin-left: 13.3vw;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-
-    a {
-      text-decoration: none;
-      color: $White;
-      &:hover {
-        text-decoration: underline;
-      }
-    }
-  }
+ }
 }
 
-  @media (max-width: 850px) {
-    footer {
-      display: none;
-      //     flex-direction: column;
-      //     justify-content: center;
-      //     align-items: center;
-      //     padding-top: 15px;
+ #Nav{
+    font-family: Dosis;
+    font-weight: 700;
+    font-size: large;
+    color: $White;
+    display: flex;
+    
 
-      //     figure {
-      //         display: none;
-      //     }
+ }
 
-      //     .infoFooter {
-      //       margin: 0;
-
-      //       img {
-      //         width: 50vw;
-      //       }
-
-      //       label {
-      //         font-size: 8vw;
-      //       }
-
-      //       p {
-      //         font-family: Dosis;
-      //         color: $White;
-      //         font-weight: bold;
-      //         font-size: 2.7vw;
-      //       }
-      //       .date {
-      //         font-size: 10vw;
-      //         line-height: 10vw;
-      //       }
-      //       a {
-      //         text-decoration: none;
-      //         color: $White;
-      //         &:hover {
-      //           text-decoration: underline;
-      //         }
-      //       }
-      //     }
-
-      //     #nav {
-      //       font-family: Dosis;
-      //       font-weight: normal;
-      //       color: $White;
-      //       margin-left: 13.3vw;
-      //     }
-    }
-  }
 </style>
